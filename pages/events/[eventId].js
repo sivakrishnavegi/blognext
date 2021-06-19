19887f1d4844e6eb30a5969c11fbcd07e4804519
sivakrishnavegi <sivakrishnavegi@yahoo.com>
2021-06-19 16:09:57 +0530

    added event detail ui

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { useRouter } from 'next/router'
+import { getEventById } from '../../data/DUMMY_DATA';
+
+import EventSummary from '../../components/eventDetail/event-summary';
+import EventLogistics from '../../components/eventDetail/event-logistics';
+import EventContent from '../../components/eventDetail/event-content'
 
 const EventDetailPage = () => {
+    const router = useRouter();
+
+    const eventId = router.query.eventId;
+    const event = getEventById(eventId);
+
+    if(!event){
+        return <p> no event found</p>
+    }
+
     return (
-        <div>
-            <h1>Event Detail Page</h1>
-        </div>
+       <Fragment>
+           <EventSummary title={event.title}/>
+           <EventLogistics  date={event.date} address={event.location} image={event.image} alt={event.title} />
+           <EventContent >
+               <p>
+                   {event.description}
+               </p>
+           </EventContent>
+       </Fragment>
     )
 }
 
