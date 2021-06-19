910edc0a029a0e36bddce790fde43efb930a1091
sivakrishnavegi <sivakrishnavegi@yahoo.com>
2021-06-19 18:04:30 +0530

    added error alert

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -2,8 +2,9 @@ import { useRouter } from 'next/router'
 import React, { Fragment } from 'react'
 import EventList from '../../components/events/EventList';
 import ResultsTitle from '../../components/events/results-title';
+import Button from '../../components/ui/Button';
 import { getFilteredEvents } from '../../data/DUMMY_DATA';
-
+import ErrorAlert from '../../components/ui/error-alert'
 const FiltredEventsPage = () => {
   const router = useRouter();
 
@@ -20,7 +21,8 @@ const FiltredEventsPage = () => {
   const numMonth =+filtredMonth;
 
 if(isNaN(numMonth) || isNaN(numYear) || numYear > 2030  || numYear < 2021 || numMonth < 0  || numMonth > 12){
-    return <p>invalid filter</p>
+    return <Fragment><ErrorAlert><p>invalid filter</p></ErrorAlert>
+    <div className="center"><Button link="/events">show all events</Button></div></Fragment>
 }
 
   const filtredEvents = getFilteredEvents({
@@ -29,7 +31,12 @@ if(isNaN(numMonth) || isNaN(numYear) || numYear > 2030  || numYear < 2021 || num
   });
 
   if(!filtredEvents || filtredEvents.length === 0){
-      return <p> no events found for th filter </p>
+      return <Fragment>
+          <ErrorAlert><p> no events found for th filter </p></ErrorAlert>
+          <div className="center">
+        <Button link="/events">show all events</Button>
+        </div>
+        </Fragment>
   }
 
 const date = new Date(numYear, numMonth-1);
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -5,6 +5,8 @@ import { getEventById } from '../../data/DUMMY_DATA';
 import EventSummary from '../../components/eventDetail/event-summary';
 import EventLogistics from '../../components/eventDetail/event-logistics';
 import EventContent from '../../components/eventDetail/event-content'
+import ErrorAlert from '../../components/ui/error-alert';
+import Button from '../../components/ui/Button';
 
 const EventDetailPage = () => {
     const router = useRouter();
@@ -13,7 +15,7 @@ const EventDetailPage = () => {
     const event = getEventById(eventId);
 
     if(!event){
-        return <p> no event found</p>
+        return <Fragment><ErrorAlert><p> no event found</p></ErrorAlert><div className='center'><Button link="/events">Show All eventes</Button></div></Fragment>
     }
 
     return (
